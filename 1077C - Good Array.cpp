f9a46f089716195bf94784e2209b33a43e0ab1da
Moulik Roy <37454620+moulik77@users.noreply.github.com>
2020-04-06 02:02:29 +0530

    Add files via upload

--- 1077C - Good Array.cpp
+++ 1077C - Good Array.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 	long long int n, i, sum;
-	vector <int> nice_indices, count(1000001, 0);
+	vector <int> count(1000001, 0), nice_indices;
 	cin>>n;
 	long long int a[n];
 	for(i=0, sum=0; i<n; i++){
@@ -15,7 +15,7 @@ int main(){
 	for(i=0; i<n; i++){
 		count[a[i]]--;
 		sum=sum-a[i];
-		if(sum%2==0 && sum/2<1000001 && count[sum/2]!=0)
+		if(sum%2==0 && sum/2<1000001 && count[sum/2]>0)
 			nice_indices.push_back(i+1);
 		sum=sum+a[i];
 		count[a[i]]++;
